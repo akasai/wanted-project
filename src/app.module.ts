0b5b9443a9d5229f630f79753a27fcfe69930598
akasai <akasai>
2024-09-28 16:33:21 +0900

    feat: 글로벌 키워드 캐시 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,31 @@
-import { Module } from '@nestjs/common'
+import { Global, Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { PrismaModule } from './common/prisma/prisma.module'
-import { CommentModule } from './modules/comment/comment.module'
+import { PrismaService } from './common/prisma/prisma.service'
 import { PostModule } from './modules/post/post.module'
 
+@Global()
 @Module({
-  imports: [PrismaModule, PostModule, CommentModule],
+  imports: [PrismaModule, PostModule],
   controllers: [AppController],
+  providers: [
+    {
+      provide: 'KEYWORD_CACHE',
+      useFactory: async (prisma: PrismaService) => {
+        console.log('Initializing keyword cache...')
+        const keywords = await prisma.keyword.findMany({
+          select: { keyword: true, author_name: true },
+        })
+        return keywords.reduce((cache, { keyword, author_name }) => {
+          if (cache.has(keyword)) cache.get(keyword).push(author_name)
+          else cache.set(keyword, [author_name])
+          return cache
+        }, new Map<string, string[]>())
+      },
+      inject: [PrismaService],
+    },
+  ],
+  exports: ['KEYWORD_CACHE'],
 })
-export class AppModule {}
+export class AppModule {
+}
